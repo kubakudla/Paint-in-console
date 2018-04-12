1d18221e785010c3858c15128ca949f7a3b51650
Jakub Kudla <kuba.kudla@stepwise.pl>
2018-04-12 17:52:31 +0200

    pom file, main class, command classes

---
+++ src/main/java/com/jkudla/command/CommandConstants.java
@@ -0,0 +1,10 @@
+package com.jkudla.command;
+
+public class CommandConstants {
+
+    public static final String CANVAS = "C";
+    public static final String LINE = "L";
+    public static final String RECTANGLE = "R";
+    public static final String BUCKET_FILL = "B";
+    public static final String QUIT = "Q";
+}
---
+++ src/main/java/com/jkudla/command/CommandController.java
@@ -0,0 +1,6 @@
+package com.jkudla.command;
+
+public class CommandController {
+
+
+}
